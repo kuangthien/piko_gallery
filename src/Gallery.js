6139cfd1e33dccb6b3aea462c3c961010d92efcb
Tran Kuang Thien <kuangthien@gmail.com>
2019-06-28 15:36:21 +0700

    user style + modal image full show

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { API_URL } from './config'
+import ModalImage from './ModalImage'
 const log = console.log
+
 const GalleryItem = props => {
+  const { tapHandlerCallback: tapHandler } = props
   return (
-    <div>
-      <a href="#" className="d-block mb-4 h-100">
-        <img className="img-fluid img-thumbnail" src={props.images.preview_webp.url} alt="" />
-      </a>
+    <div className="galItem">
+      <div className="d-block mb-4 h-100">
+        <b
+          onClick={() => tapHandler(props.images.original.url)}
+          className="img"
+          style={{
+            backgroundImage: `url(${props.images.fixed_width.url})`,
+          }}
+        />
+        {props.user && (
+          <b className="u d-block text-left mt-2">
+            <b
+              className="u-avat mr-1"
+              style={{
+                borderRadius: '50%',
+                border: '2px solid #fff',
+                backgroundPosition: '50%',
+                backgroundSize: 'cover',
+                height: 24,
+                width: 24,
+                display: 'inline-block',
+                verticalAlign: 'middle',
+                backgroundImage: `url(${props.user.avatar_url})`,
+              }}
+            />
+            <b className="small">{props.user.display_name}</b>
+          </b>
+        )}
+      </div>
     </div>
   )
 }
 const Gallery = () => {
+  const tapHandler = url => {
+    setShowImageFullUrl(url)
+  }
   const [arrImages, setArrImages] = useState([])
+  const [showImageFullUrl, setShowImageFullUrl] = useState(null)
+
   useEffect(() => {
     fetch(API_URL)
       .then(rs => rs.text())
@@ -23,6 +56,26 @@ const Gallery = () => {
   const displayArr = arrImages || []
   return (
     <div className="container">
+      <ModalImage urlShouldShow={showImageFullUrl} />
+
+      <style>
+        {`
+            body{
+                background: #eee;
+            }
+          .galItem .img{
+              height: 200px;
+              width:100%;
+              background-size:cover;
+              background-position: 50%;
+              display:block;
+              border: 8px solid #fff;
+              border-radius: 2px;
+              box-shadow: 0 0 2px #ccc
+          }
+          
+        `}
+      </style>
       <h1 className="font-weight-light text-center text-lg-left mt-4 mb-0">Thumbnail Gallery</h1>
 
       <hr className="mt-2 mb-5" />
@@ -32,7 +85,7 @@ const Gallery = () => {
           displayArr.map((v, i) => {
             return (
               <div key={i} className="col-lg-3 col-md-4 col-6">
-                <GalleryItem {...v} />
+                <GalleryItem {...v} tapHandlerCallback={tapHandler} />
               </div>
             )
           })}
---
+++ src/ModalImage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ModalImage = props => {
+  const { urlShouldShow } = props
+  return <div>{urlShouldShow}</div>
+}
+
+export default ModalImage
